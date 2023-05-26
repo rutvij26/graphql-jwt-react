4b3b82cdb5b59a3aeac151d7ba8c798226634cdf
Rutvij Sathe <rut26sathe@gmail.com>
2023-05-25 20:18:26 -0400

    register page

--- web/src/pages/Register.tsx
+++ web/src/pages/Register.tsx
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Register: React.FC = () => {
-    return <div>Register Page</div>;
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <form
+            onSubmit={(e) => {
+                e.preventDefault();
+                console.log('Form Submitted');
+                console.log(email, password);
+            }}
+        >
+            <div className="">
+                <input
+                    value={email}
+                    placeholder="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </div>
+            <div className="">
+                <input
+                    value={password}
+                    placeholder="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    type="password"
+                />
+            </div>
+            <button type="submit">Register</button>
+        </form>
+    );
 };
