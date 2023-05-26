15b6e51ef1fc2747ec825c057a18eb3e9e7910b0
Rutvij Sathe <rut26sathe@gmail.com>
2023-05-25 20:13:12 -0400

    Links created for routes

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -3,12 +3,30 @@ import {
     BrowserRouter as Router,
     Routes as _Routes,
     Route,
+    Link,
 } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Register } from './pages/Register';
+import { Login } from './pages/Login';
+
 export const Routes: React.FC = () => {
     return (
         <Router>
+            <header>
+                <div className="">
+                    <Link to="/"> home</Link>
+                </div>
+                <div className="">
+                    <Link to="/register"> register</Link>
+                </div>
+                <div className="">
+                    <Link to="/login"> login</Link>
+                </div>
+            </header>
             <_Routes>
-                <Route path="/" element={<div>hi</div>} />
+                <Route path="/" element={<Home />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
             </_Routes>
         </Router>
     );
